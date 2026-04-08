1d60c86369f7114aa869adb462cc5dca228a4e22
Patrick Neves <pneves@my.centennialcollege.ca>
2026-04-07 23:39:50 -0300

    add StartsWithUpper extension method and pipeline file

--- StringLibrary/Class1.cs
+++ StringLibrary/Class1.cs
@@ -1,7 +1,12 @@
-﻿namespace StringLibrary
-{
-    public class Class1
-    {
+﻿namespace UtilityLibraries;
 
+public static class StringLibrary
+{
+    public static bool StartsWithUpper(this string? str)
+    {
+        if (string.IsNullOrWhiteSpace(str))
+            return false;
+
+        return char.IsUpper(str[0]);
     }
 }
